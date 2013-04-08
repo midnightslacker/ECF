2d98ae3b24757143913b201d79451d14e9051a42
Midnightslacker <midnightslacker11@gmail.com>
2013-04-07 21:27:09 -0400

    Added Remaining Schedule and PrintSchedule Functions
    TODO: Logic has to be added to make sure teams can't play the same
    person in the same week (i.e. team 1 vs team 2 and team 1 vs team 3)

--- createECFSchedule3.0.py
+++ createECFSchedule3.0.py
@@ -9,9 +9,6 @@ Considerations: Weeks 1-3 and Weeks 11-13 are special cases.
 
 import sys, random
 
-#Create a list for number of teams
-seq = [1,2,3,4,5,6,7,8,9,10,11,12]
-
 #Create the dictionary/hash of owners
 owners = {1:"Christ",
           2:"Gruver",
@@ -26,12 +23,48 @@ owners = {1:"Christ",
           11:"Sicher",
           12:"Void"}
 
-#Create divisions for excluding from remaining schedule
-dNorth = ["Miller", "Millen", "Void"]
-dEast  = ["Christ", "Sicher", "Doncsecz"]
-dWest  = ["Nick A.", "Powell", "Huntington"]
-dSouth = ["Ressler", "Pal", "Gruver"]
 
 
 #create sequence for remaing games for each owner.
 christ_seq = [3,4,6,7,8,10,12]
+gruver_seq = [3,4,5,9,10,11,12]
+miller_seq = [1,2,5,7,8,9,11]
+millen_seq = [1,2,6,8,9,10,11]
+hunt_seq   = [2,3,7,8,9,11,12]
+nickA_seq  = [1,4,7,8,9,11,12]
+ressler_seq= [1,3,5,6,9,10,11]
+pal_seq    = [1,3,4,5,6,10,12]
+donch_seq  = [2,3,4,5,6,7,10]
+powell_seq = [1,2,4,7,8,9,12]
+sicher_seq = [2,3,4,5,6,7,12]
+void_seq   = [1,2,5,6,8,10,11]
+
+#create a list of lists
+ownerSched = [christ_seq, gruver_seq, miller_seq, millen_seq, hunt_seq,
+              nickA_seq, ressler_seq, pal_seq, donch_seq, powell_seq,
+              sicher_seq, void_seq]
+
+
+def RemainingSchedule( ownersName, seq ):
+    print "** " + str(ownersName) + "'s" + " Schedule **"
+    week = 4;
+
+    for opp in random.sample( seq, 7):
+        print "Week " + str(week) + ": " + str( owners.get(opp) )
+        week += 1
+        
+def PrintSchedule():
+    ownerVal = 0
+    for owner in owners:
+        RemainingSchedule( owners.get(owner) , ownerSched[ownerVal] )
+        if ownerVal < 11:
+            ownerVal += 1
+
+PrintSchedule()
+
+
+
+
+
+
+
